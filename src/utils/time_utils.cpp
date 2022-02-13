15cdd38576b032afa87d993076ae8bfe64b80885
Mykhailo Didur <mikhaildidur2003@gmail.com>
2022-02-14 00:09:25 +0200

    Use non-readable timestamp in milliseconds from start of epoch

--- src/utils/time_utils.cpp
+++ src/utils/time_utils.cpp
@@ -1,9 +1,9 @@
 #include "time_utils.h"
 
 string time_utils::timestamp() {
-    auto now = chrono::system_clock::now();
-    auto in_time_t = chrono::system_clock::to_time_t(now);
-    stringstream ss;
-    ss << put_time(localtime(&in_time_t), "%Y-%m-%d %X");
-    return ss.str();
+    chrono::milliseconds timestamp =
+        chrono::duration_cast<chrono::milliseconds>(
+        chrono::system_clock::now().time_since_epoch()
+    );
+    return to_string(timestamp.count());
 }
